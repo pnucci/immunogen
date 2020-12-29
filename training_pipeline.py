3eef3002503a8c1eabcf48fd17f27a0017080206
Pedro Nucci <pnuccipl@gmail.com>
2020-12-29 22:37:02 +0100

    Clean code

--- training_pipeline.py
+++ training_pipeline.py
@@ -23,62 +23,47 @@ from sklearn.metrics import average_precision_score, make_scorer
 solution = lstm_solution
 
 RANDOM_SEED = 0
-N_SPLITS = 5
-N_REPEATS = 2
-early_stop_metric = 'val_loss'
 
-# %%
+# outer cv
+cv_oos_performance = RepeatedStratifiedKFold(
+    n_splits=3,
+    n_repeats=1,
+    random_state=RANDOM_SEED
+)
+
+# inner cv
+cv_hyperparams = RepeatedStratifiedKFold(
+    n_splits=3,
+    n_repeats=1,
+    random_state=RANDOM_SEED
+)
+
 
 print(dataset['y'].value_counts())
 
-test_split = StratifiedShuffleSplit(
-    n_splits=1, test_size=0.3).split(dataset.X, dataset.y)
-train_ix, test_ix = list(test_split)[0]
-train = dataset.iloc[train_ix]
-test = dataset.iloc[test_ix]
+X = np.stack(dataset.X.values)
+y = dataset.y.values
+y_cat = utils.to_categorical(y)
 
-# %%
-
-train_prop = train.y.value_counts()/len(train)
-test_prop = test.y.value_counts()/len(test)
-class_proportion_preserved = np.allclose(train_prop, test_prop, atol=0.01)
-assert(class_proportion_preserved)
-
-# %%
-
-train_X = np.stack(train.X.values)
-train_y = train.y.values
-train_y_cat = utils.to_categorical(train.y.values)
-test_X = np.stack(test.X.values)
-test_y = test.y.values
-test_y_cat = utils.to_categorical(test.y.values)
-
-print('train_X.shape', train_X.shape)
-print(train_X.shape, train_y.shape)
-print(test_X.shape, test_y.shape)
-classes = np.unique(train_y)
-num_outputs = len(classes)
+num_outputs = len(np.unique(y))
 print('num_outputs', num_outputs)
 
-aupr = keras.metrics.AUC(
-    curve="PR",
-    name='aupr',
-)
-auroc = keras.metrics.AUC(
-    curve="ROC",
-    name='auroc',
-)
 
-
-def build_model():
+def build_model(hidden_layers, learning_rate) -> KerasClassifier:
+    print(learning_rate)
     model = solution.build_model()
     model.add(
         layers.Dense(units=num_outputs, activation='softmax')
     )
+    from tensorflow.keras.optimizers import Adam
     model.compile(
-        optimizer='adam',
+        # optimizer='adam',
+        optimizer=Adam(learning_rate=learning_rate),
         loss='categorical_crossentropy',
-        metrics=[aupr, auroc],
+        metrics=[
+            keras.metrics.AUC(curve="PR", name='aupr'),
+            keras.metrics.AUC(curve="ROC", name='auroc')
+        ],
     )
     return model
 
@@ -91,40 +76,49 @@ pipeline = Pipeline([
         verbose=1
     ))
 ])
-kfold = RepeatedStratifiedKFold(
-    n_splits=N_SPLITS,
-    n_repeats=N_REPEATS,
-    random_state=RANDOM_SEED
-)
+
 hyperparam_search = RandomizedSearchCV(
     estimator=pipeline,
     n_iter=2,
     scoring=['average_precision', 'roc_auc'],
     param_distributions=dict(
-        model__batch_size=[50, 100],
-        model__epochs=[20, 30]
+        model__batch_size=[25, 50, 100],
+        model__epochs=[10, 20, 50],
+        model__hidden_layers=[20],
+        model__learning_rate=[0.01,0.005,0.001]
+
     ),
     refit='roc_auc',
     # n_jobs=-1,
-    cv=kfold
+    cv=cv_hyperparams
 )
-hyperparam_search_result = hyperparam_search.fit(train_X, train_y)
+# %%
+oos_performance = cross_validate(
+    estimator=hyperparam_search,
+    X=X,
+    y=y,
+    cv=cv_oos_performance,
+    return_estimator=True
+)
+#%%
+print(oos_performance)
+pd.DataFrame(oos_performance)
 
-# %%
-print("Best: %f using %s" % (hyperparam_search_result.best_score_,
-                             hyperparam_search_result.best_params_))
-means = hyperparam_search_result.cv_results_['mean_test_roc_auc']
-stds = hyperparam_search_result.cv_results_['std_test_roc_auc']
-params = hyperparam_search_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
-results_df = pd.DataFrame(hyperparam_search_result.cv_results_)
-results_df.sort_values('rank_test_roc_auc')[
-    ['params', 'mean_test_roc_auc', 'std_test_roc_auc']]
-# %%
-best = hyperparam_search_result.best_estimator_
-preds = best.predict_proba(test_X)
-preds
-# %%
-roc_auc_score(test_y_cat, preds)
-# %%
+# hyperparam_search_result = hyperparam_search.fit(train_X, train_y)
+# # %%
+# print("Best: %f using %s" % (hyperparam_search_result.best_score_,
+#                              hyperparam_search_result.best_params_))
+# means = hyperparam_search_result.cv_results_['mean_test_roc_auc']
+# stds = hyperparam_search_result.cv_results_['std_test_roc_auc']
+# params = hyperparam_search_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
+# results_df = pd.DataFrame(hyperparam_search_result.cv_results_)
+# results_df.sort_values('rank_test_roc_auc')[
+#     ['params', 'mean_test_roc_auc', 'std_test_roc_auc']]
+# # %%
+# best = hyperparam_search_result.best_estimator_
+# preds = best.predict_proba(test_X)
+# preds
+# # %%
+# roc_auc_score(test_y_cat, preds)
